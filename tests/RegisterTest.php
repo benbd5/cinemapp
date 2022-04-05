5bafeea4dd25849308a76eebcfb105204677a501
Benjamin Belz <belzbenjaminp@gmail.com>
2022-04-05 16:54:55 +0200

    Ajout de tests pour vérifier que les pages inscriptions et connexion s'affichent bien et que la connexion est bien possible

---
+++ tests/RegisterTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class RegisterTest extends WebTestCase
+{
+    // Test d'affichage de la page login
+    public function testGetLogin()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/login');
+
+        $this->assertResponseStatusCodeSame(200);
+    }
+
+    // Test du login avec un utilisateur existant
+    public function testPostLogin()
+    {
+        $client = static::createClient();
+
+        // On récupère la page login avec le crawler
+        $crawler = $client->request('GET', '/login');
+
+        // On récupère le bouton login et on passe les informations du formulaire
+        $form = $crawler->selectButton('Login')->form(['_username' => 'test@test.fr', '_password' => '123456']);
+        $client->submit($form);
+        $client->followRedirect();
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    // Test d'affichage de la page register
+    public function testGetRegister()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/register');
+
+        $this->assertResponseStatusCodeSame(200);
+    }
+}
